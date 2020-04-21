7971e0186f190d0c91196d12d6fe44c848517060
Žan Ožbot <zan.ozbot@gmail.com>
2020-04-21 22:42:31 +0200

    Implemented scraping for douyin, vt and tiktok

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,9 +5,9 @@ const axios = require("axios")
 const cheerio = require("cheerio")
 const userAgent = new UserAgent({ deviceCategory: 'mobile' })
 
-exports.downloadVideo = functions.https.onCall(async (clientData, context) => {
+exports.downloadTikTokVideo = functions.https.onCall(async (clientData, context) => {
   const { data } = await axios.get(
-    'https://www.tiktok.com/@deborahmilani/video/6808878154556509446',
+    clientData.url,
     {
       headers: { 'User-Agent': userAgent.data.userAgent }
     }
@@ -15,27 +15,78 @@ exports.downloadVideo = functions.https.onCall(async (clientData, context) => {
 
   const $ = await cheerio.load(data);
 
-  const videoObject = $('#videoObject').get();
+  const videoObject = await $('#videoObject').get();
 
   if (videoObject.length === 0) {
-    return Promise.reject('video-not-found');
+    return Promise.resolve('video-not-found');
   }
 
   const content = JSON.parse(videoObject[0].children[0].data);
-
-  const interactions = content.interactionStatistic.map((interaction: any) => {
-    const type = interaction.interactionType['@type'].substr('http://schema.org/'.length);
-    return { type, count: interaction.userInteractionCount };
-  });
 
   const video = {
     name: content.creator.name,
     handle: '@' + content.creator.alternateName,
     description: content.description.substr(content.description.indexOf('.') + 1).trim(),
-    uploadDate: content.uploadDate,
-    videoUrl: content.contentUrl,
-    interactions
+    videoUrl: content.contentUrl
   }
 
   return Promise.resolve(video);
+});
+
+
+exports.downloadTikTokVtVideo = functions.https.onCall(async (clientData, context) => {
+  const { data } = await axios.get(
+    clientData.url,
+    {
+      headers: { 'User-Agent': userAgent.data.userAgent }
+    }
+  );
+
+  const $ = await cheerio.load(data);
+
+  const description = $('meta[property="og:description"]').attr('content');
+
+  const scripts = (await $('script').get()).filter((s: any) => s.children.length > 0 && s.children[0].data.indexOf('window.__INIT_PROPS__') >= 0);;
+
+  if (scripts.length === 0) {
+    return Promise.resolve('video-not-found');
+  }
+
+  const content = scripts[0].children[0].data.replace('window.__INIT_PROPS__ =', '').trim();
+
+  const json = JSON.parse(content);
+  const videoProps = json['/i18n/share/video/:id'].videoObjectPageProps.videoProps;
+
+  return Promise.resolve(
+    {
+      name: videoProps.creator.name,
+      handle: '@' + videoProps.creator.alternateName,
+      description,
+      videoUrl: videoProps.contentUrl
+    }
+  );
+});
+
+exports.downloadDouyinVideo = functions.https.onCall(async (clientData, context) => {
+  const { data } = await axios.get(
+    clientData.url,
+    {
+      headers: { 'User-Agent': userAgent.data.userAgent }
+    }
+  );
+
+  const $ = await cheerio.load(data);
+
+  const user = $('p.bottom-user').text();
+  const description = $('p.bottom-desc').text();
+  const videoUrl = $('#theVideo').attr('src');
+
+  return Promise.resolve(
+    {
+      name: user,
+      handle: '@' + user,
+      description,
+      videoUrl
+    }
+  );
 });
