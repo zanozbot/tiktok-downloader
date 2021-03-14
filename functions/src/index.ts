36bfdb125e980b9e46375fee8ea1300b7e6d122f
Žan Ožbot <zan.ozbot@gmail.com>
2021-03-14 21:07:45 +0100

    Fixed downloading of TikTok videos

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,44 +1,45 @@
-import * as functions from 'firebase-functions';
+const functions = require("firebase-functions");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
 
-const cheerio = require("cheerio")
-const needle = require('needle');
+const stream = require("stream");
+const { promisify } = require("util");
+const got = require("got");
 
-exports.downloadTikTokVideo = functions.https.onCall(async (clientData, context) => {
-  console.log(clientData.url);
+const TikTokScraper = require("tiktok-scraper");
 
-  const response = await needle('get', clientData.url, null, {
-    headers: {
-      user_agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.34',
-      accept: 'text/html,application/xhtml+xml'
-    },
-    follow_max: 5
-  });
+const pipeline = promisify(stream.pipeline);
+const app = express();
 
-  const $ = await cheerio.load(response.body);
+app.get("/", async (req: any, res: any) => {
+  const randomString = Array.from(
+    { length: 4 },
+    () => Math.random().toString(36)[2]
+  ).join("");
 
-  const content$ = $('#__NEXT_DATA__').get();
+  const headers = {
+    "User-Agent": randomString,
+    Referer: "https://www.tiktok.com/",
+    Cookie: "tt_webid_v2=" + randomString
+  };
 
-  const content = content$[0].children[0].data;
+  const videoMeta = await TikTokScraper.getVideoMeta(
+    "https://www.tiktok.com/@venera_creations/video/6925128747318463750",
+    headers
+  );
 
-  const json = JSON.parse(content);
+  const url = videoMeta.collector[0].videoUrl;
+  const fileName = `${videoMeta.collector[0].authorMeta.name}_${videoMeta.collector[0].id}.mp4`;
+  const tempFilePath = path.join(os.tmpdir(), fileName);
 
-  const videoUrl = json.props.pageProps.videoData.itemInfos.video.urls[0];
+  await pipeline(
+    got.stream(url, { headers: videoMeta.headers }),
+    fs.createWriteStream(tempFilePath)
+  );
 
-  if (!videoUrl) {
-    return Promise.resolve({ status: 'video-not-found' });
-  }
-
-  const name = json.props.pageProps.videoData.authorInfos.nickName;
-  const handle = '@' + json.props.pageProps.videoData.authorInfos.uniqueId;
-  const description = json.props.pageProps.videoData.itemInfos.text;
-
-  const video = {
-    status: 'ok',
-    name,
-    handle,
-    description,
-    videoUrl
-  }
-
-  return Promise.resolve(video);
+  res.download(tempFilePath);
 });
+
+exports.downloadTikTokVideo = functions.https.onRequest(app);
